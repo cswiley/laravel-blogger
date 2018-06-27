b611d00ac3e1deb1d8230a9b56743de93d77ee08
cameron wiley <cswiley@gmail.com>
2018-06-27 09:38:41 -0700

    namespace updates

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 Auth::routes();
 
-Route::delete('api/blog-image', '\Cswiley\Blogging\Controllers\API\BlogImageController@delete');
-Route::resource('api/blog-image', '\Cswiley\Blogging\Controllers\API\BlogImageController')->only([
+Route::delete('api/blog-image', '\Cswiley\Blogger\Controllers\API\BlogImageController@delete');
+Route::resource('api/blog-image', '\Cswiley\Blogger\Controllers\API\BlogImageController')->only([
     'create',
     'store'
 ]);
@@ -13,10 +13,10 @@ Route::middleware([
     'auth'
 ])->group(function () {
 });
-Route::post(config('blog.prefix') . '/preview/{id}', '\Cswiley\Blogging\Controllers\BlogController@preview');
-Route::resource(config('blog.prefix'), '\Cswiley\Blogging\Controllers\BlogController');
+Route::post(config('blog.prefix') . '/preview/{id}', '\Cswiley\Blogger\Controllers\BlogController@preview');
+Route::resource(config('blog.prefix'), '\Cswiley\Blogger\Controllers\BlogController');
 
 
-Route::delete('api/' . config('blog.prefix') .  '/upload', '\Cswiley\Blogging\Controllers\API\BlogController@deleteImage');
-Route::post('api/' . config('blog.prefix') . '/upload', '\Cswiley\Blogging\Controllers\API\BlogController@storeImage');
-Route::resource('api/' . config('blog.prefix'), '\Cswiley\Blogging\Controllers\API\BlogController');
+Route::delete('api/' . config('blog.prefix') .  '/upload', '\Cswiley\Blogger\Controllers\API\BlogController@deleteImage');
+Route::post('api/' . config('blog.prefix') . '/upload', '\Cswiley\Blogger\Controllers\API\BlogController@storeImage');
+Route::resource('api/' . config('blog.prefix'), '\Cswiley\Blogger\Controllers\API\BlogController');
