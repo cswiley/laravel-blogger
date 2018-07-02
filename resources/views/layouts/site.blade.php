27cab43c4e2850c9efb3acf7f2d7015df3537ef2
cameron wiley <cswiley@gmail.com>
2018-07-02 09:45:54 -0700

    blade updates for metas

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,11 +1,9 @@
 @extends('blogger::layouts.base')
 
 @section('main')
-    @if(!empty($menu))
     <div class="fixed-header bg-primary">
         @include('blogger::partials.menu')
     </div>
-    @endif
     <div class="content margin-vertical-1">
         @yield('content')
     </div>
