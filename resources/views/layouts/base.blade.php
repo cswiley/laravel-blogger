a90a984b7aa8b9e7e75efe05958a9691e3343057
cameron wiley <cswiley@gmail.com>
2018-07-02 09:42:49 -0700

    blade updates for metas

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head lang="en-US">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta property="og:url" content="{{ env('APP_URL')  }}"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:title" content="Title Here"/>
-    <meta property="og:description" content="Description Here"/>
-    <meta property="og:image" content="Image Here"/>
-    <title>Title Here</title>
+    @include('blogger::partials.metas')
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
           integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}"/>
---
+++ resources/views/partials/metas.blade.php
@@ -0,0 +1,18 @@
+<!-- Google Analytics -->
+<!-- End Google Analytics -->
+
+<!-- Google Analytics -->
+<!-- End Google Analytics -->
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta property="og:url" content="{{ env('APP_URL')  }}"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="Title Here"/>
+<meta property="og:description" content="Description Here"/>
+<meta property="og:image" content="Image Here"/>
+<meta property="og:image:width" content="1280"/>
+<meta property="og:image:height" content="677"/>
+<title>Title Here</title>
