75a74364277034fff58743c8a7054a4b66839718
cameron wiley <cswiley@gmail.com>
2018-07-05 18:04:21 -0700

    repo fix

--- src/Repositories/BlogRepository.php
+++ src/Repositories/BlogRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Cswiley\Blogger\Repositories;
 
 use Carbon\Carbon;
@@ -7,28 +8,34 @@ use Cswiley\Blogger\Models\Blog;
 class BlogRepository
 {
     protected $query;
+    protected $model;
 
     public function __construct(Blog $blog)
     {
+        $this->model = $blog;
         $this->query = $blog;
     }
 
     public function onlyActive()
     {
         $this->query = $this->query->where('published_at', '<', Carbon::now())->where('visibility', Blog::VISIBILITY_PUBLIC);
+
         return $this;
     }
 
     public function idOrSlug($value)
     {
-        $this->query =  $this->query->where('id', $value)->orWhere('slug', $value);
+        $this->query = $this->query->where('id', $value)->orWhere('slug', $value);
+
         return $this;
     }
 
     public function execute()
     {
-        return $this->query;
+        $query       = $this->query;
+        $this->query = $this->model;
 
+        return $query;
     }
 
 }
